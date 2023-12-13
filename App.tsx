f042efe1a8098a20b4a26c08a366dd4dc2d5b066
Miguel Marçola <miguelmarcola@gmail.com>
2023-12-13 17:58:57 -0300

    feat: creating login page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { Text, View, StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import { Box, NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 import { Signin } from '@screens/Signin';
@@ -7,12 +11,16 @@ import { Signin } from '@screens/Signin';
 import { THEME } from './src/theme';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Signin /> : <Loading /> }
-    </NativeBaseProvider>
-  );
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            {fontsLoaded ? <Signin /> : <Loading />}
+        </NativeBaseProvider>
+    );
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,10 +1,69 @@
-import { Image, VStack } from "native-base";
-import BackgroungImg from "@assets/background.png"
+import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import BackgroungImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function Signin() {
     return (
-        <VStack flex={1} bg="gray.700">
-            <Image source={BackgroungImg} alt="Background screen" resizeMode="contain" position="absolute" />
-        </VStack>
-    )
+        <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}
+            scrollEnabled={false}
+        >
+            <VStack
+                flex={1}
+                bg="gray.700"
+                px={10}
+                justifyContent="space-between"
+            >
+                <Image
+                    source={BackgroungImg}
+                    alt="Background screen"
+                    resizeMode="contain"
+                    position="absolute"
+                />
+
+                <Center>
+                    <LogoSvg />
+                    <Text color="gray.100" fontSize="sm">
+                        Treine sua mente e seu corpo
+                    </Text>
+                </Center>
+
+                <Center>
+                    <Heading
+                        color="gray.100"
+                        fontSize="xl"
+                        mb={6}
+                        fontFamily="heading"
+                    >
+                        Acesse sua conta
+                    </Heading>
+
+                    <Input
+                        placeholder="E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    <Input placeholder="Senha" secureTextEntry />
+
+                    <Button title="Acessar" />
+                </Center>
+
+                <Center mb={12}>
+                    <Text
+                        color="gray.100"
+                        fontSize="sm"
+                        mb={3}
+                        fontFamily="body"
+                    >
+                        Ainda não tem acesso?
+                    </Text>
+
+                    <Button title="Criar conta" variant="outline" />
+                </Center>
+            </VStack>
+        </ScrollView>
+    );
 }
